34a5a4cf0198adebf0fb8fa23d190ad9f305f4ae
flynnjim <flynnlomas@hotmail.co.uk>
2025-01-20 16:17:15 +0000

    completed series of katas

---
+++ 25/01-jan/powers-of-two/powers-of-two.js
@@ -0,0 +1,29 @@
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+*/
+
+function powersOfTwo(n) {
+  const powerTwo = [];
+  for (let i = 0; i <= n; i++) {
+    powerTwo.push(Math.pow(2, i));
+  }
+  return powerTwo;
+}
+/*
+// Alternatively
+function powersOfTwo(n) {
+  const powersOfTwo = [];
+  let pushPower = 1;
+  for (let i = 0; i <= n; i++) {
+    powersOfTwo.push(pushPower);
+    pushPower *= 2;
+  }
+  return powersOfTwo;
+}
+*/
